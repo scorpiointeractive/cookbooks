783b982e32e0bbf1f41608bf5fff4393bd25ec82
Jesse "tux" Estomago <jesse@scorp.io>
2013-09-11 08:37:12 +0200

    Update mod_rpaf

--- apache2/recipes/mod_rpaf.rb
+++ apache2/recipes/mod_rpaf.rb
@@ -5,12 +5,39 @@
 
 case node[:platform]
 when "debian","ubuntu"
-	package "libapache2-mod-rpaf"
+  package "libapache2-mod-rpaf"
 
 when "redhat","centos","oracle","amazon","arch"
+  rpaf_url = "http://stderr.net/apache/rpaf/download/mod_rpaf-0.6.tar.gz"
+  src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/mod_rpaf-0.6.tar.gz"
 
+  packages = value_for_platform(
+    ["centos","redhat","fedora","amazon","scientific","arch"] => {'default' => ['httpd-devel']},
+    ["ubuntu","debian"] => {"default" => ['apache2-dev']},
+    "default" => []
+  )
+ 
+  packages.each do |devpkg|
+    package devpkg
+  end
+
+  remote_file rpaf_url do
+    source rpaf_url
+    path src_filepath
+    backup false
+  end
+
+  bash "compile mod_rpaf" do
+    cwd ::File.dirname(src_filepath)
+    code <<-EOH
+      tar zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)} &&
+      cd mod_rpaf-0.6 &&
+      apxs -i -c -n mod_rpaf-2.0.so mod_rpaf-2.0.c
+    EOH
+  end
 end
  
 apache_module "rpaf" do 
+  filename "mod_rpaf-2.0.so"
   conf true
 end
