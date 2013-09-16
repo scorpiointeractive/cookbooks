5ee4517a33fbc86558162475f7fa6e6c8024ba3f
Jesse "tux" Estomago <jesse@scorp.io>
2013-09-16 10:03:53 +0200

    Used ROS for mod_cloudflare

--- apache2/recipes/mod_cloudflare.rb
+++ apache2/recipes/mod_cloudflare.rb
@@ -8,15 +8,6 @@ when "debian","ubuntu"
   package "libapache2-mod-rpaf"
 
 when "redhat","centos","oracle","amazon","arch"
-  cloudflare_url = "https://raw.github.com/cloudflare/mod_cloudflare/master/mod_cloudflare.c"
-  src_filepath  = "/tmp/mod_cloudflare.c"
-  src_dir = File.dirname(src_filepath)
-  basename = File.basename(src_filepath)
-  
-  log "  CloudFlare Url = #{cloudflare_url}"
-  log "  Source Path = #{src_filepath}"
-  log "  Source Directory = #{src_dir}"
-  log "  Filename = #{basename}"
 
   packages = value_for_platform(
     ["centos","redhat","fedora","amazon","scientific","arch"] => {'default' => ['httpd-devel']},
@@ -33,11 +24,38 @@ when "redhat","centos","oracle","amazon","arch"
   #  path src_filepath
   #  backup false
   # end
+
+  prefix			= node[:scorpio_defaults][:mod_rpaf_backup_prefix]
+  storage_provider 		= node[:scorpio_defaults][:storage_provider]
+  storage_container 		= node[:scorpio_defaults][:storage_container]
+  dst_filepath  	 	= "/tmp/mod_cloudflare.tar.gz"
+  dst_dir 			= File.dirname(dst_filepath)
+  basename 			= File.basename(dst_filepath)
+  storage_accnt_id 		= node[:scorpio_defaults][:storage_accnt_id]
+  storage_accnt_secret 		= node[:scorpio_defaults][:storage_accnt_secret]
+  
+  command_to_execute = "/opt/rightscale/sandbox/bin/ros_util get" +
+    " --cloud #{storage_provider} --container #{storage_container}" +
+    " --dest #{dst_filepath}" +
+    " --source #{prefix} --latest"
+  
+  options = {}
+
+  environment_variables = {
+    'STORAGE_ACCOUNT_ID' => storage_accnt_id,
+    'STORAGE_ACCOUNT_SECRET' => storage_accnt_secret
+  }.merge(options)
+
+  execute "Download mod_rpaf from Remote Object Store" do
+    command command_to_execute
+    creates dst_filepath
+    environment environment_variables
+  end
   
   bash "compile mod_cloudflare" do
-    cwd ::File.dirname(src_filepath)
+    cwd dst_dir
     code <<-EOH
-      wget #{cloudflare_url} && 
+      tar zxf #{basename} &&
       apxs -i -c -n mod_cloudflare.so mod_cloudflare.c
     EOH
   end
